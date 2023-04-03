731daea90c5999c1cf863c24398dec0ed5d467ea
Batov-Anatoliy <play.hoffa@gmail.com>
2023-04-03 19:42:43 +0300

    new-commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b ** 2) - 4 * a * c;
+
+  if (discriminant === 0) {
+    let formulaRoot = (b * -1) / (2 * a);
+    arr.push(formulaRoot);
+  }
+  if (discriminant > 0) {
+    let formulaRootPlus = ((b * -1) + Math.sqrt(discriminant)) / (2 * a);
+    arr.push(formulaRootPlus);
+    let formulaRootMinus = ((b * -1) - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(formulaRootMinus);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentPerMounth = percent / 100 / 12;
+  let creditSum = amount - contribution;
+  let monthlyPayment =
+    creditSum *
+    (percentPerMounth +
+      (percentPerMounth / (((1 + percentPerMounth) ** countMonths) - 1)));
+  let grandTotal = monthlyPayment * countMonths;
+  grandTotal = grandTotal.toFixed(2);
+  grandTotal = +grandTotal;
+  return grandTotal;
 }
