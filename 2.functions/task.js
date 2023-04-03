a3066531d411c9870ec50978be583039a7a13e2c
Batov-Anatoliy <play.hoffa@gmail.com>
2023-04-03 19:55:16 +0300

    new-commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,16 @@
 function getArrayParams(...arr) {
-
+  let min = arr[0];
+  min = Math.min(...arr);
+  
+  let max = arr[0];
+  max = Math.max(...arr);
+  
+  let sum = arr.reduce(function(sumOfNumbers, element) {
+    return sumOfNumbers + element;
+  }, 0);
+  let quantityElement = arr.length;
+  let avg = sum / quantityElement;
+  avg = +avg.toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
