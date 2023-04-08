d2e20b6d5bb37df0e5700d6bf583efa828975d00
Batov-Anatoliy <play.hoffa@gmail.com>
2023-04-08 18:18:40 +0300

    new-commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+    let comparisonresult = arr1.every((n, i) => n === arr2[i]);
+  return comparisonresult;
+
+
+
+//   const compareArrays = (a, b) => a.length === b.length && a.every((n, i) => n === b[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
