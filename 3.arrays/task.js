af078ed81a1a3543dd0a9844aa6c866607fd77b5
Batov-Anatoliy <play.hoffa@gmail.com>
2023-04-11 20:29:37 +0300

    new-commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,28 @@
 function compareArrays(arr1, arr2) {
-    let comparisonResult;
-    if (arr1.length === arr2.length) {
+  let comparisonResult;
+  if (arr1.length === arr2.length) {
     comparisonResult = arr1.every((n, i) => n === arr2[i]);
-    } else {
-        return comparisonResult = false;
-    }
+  } else {
+    return (comparisonResult = false);
+  }
   return comparisonResult;
-
-
-
-//   const compareArrays = (a, b) => a.length === b.length && a.every((n, i) => n === b[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (users.length === 0) {
+    return (result = 0);
+  }
+  if (gender === "мужской" || gender === "женский") {
+  const sortingByGender = users.filter((obj) => obj.gender === gender);
+  const agesQuantity = sortingByGender.map((obj) => obj.age);
+  const arrayWithAges = agesQuantity.reduce(
+    (total, element) => total + element,
+    0
+  );
+  const elementsQuantity = agesQuantity.length;
+  const result = arrayWithAges / elementsQuantity;
+  return result;
+  } else {
+    return (result = 0);
+  }
 }
