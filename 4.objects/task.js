a88b5ee014868c9f83ceaf94509018379e5b2b8b
Batov-Anatoliy <play.hoffa@gmail.com>
2023-04-20 10:11:59 +0300

    new-commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = new Array();
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  if ('marks' in Student === true) {
+    this.marks.push(marksToAdd);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if ('marks' in Student === false || this.marks.length === 0) {
+    return 0;
+  }
+  let sumOfRatings = this.marks.reduce(function (quantity, element) {
+    quantity + element
+  }, 0);
+  let numberOfRatings = this.marks.length;
+  this.averageRating = sumOfRatings / numberOfRatings;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
